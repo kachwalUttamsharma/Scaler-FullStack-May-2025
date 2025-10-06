4981a17721f53a32e1156ce2990c1c0658814c32
kachwalUttamsharma <kachwaluttamsharma@gmail.com>
2025-10-06 08:02:42 +0530

    product service

--- NodeJs/class2/ProductService/server.js
+++ NodeJs/class2/ProductService/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
-const ProductModel = require("./models/Product");
+const productRoutes = require("./routes/productRoutes");
 
 const app = express();
 const port = 3000;
@@ -9,43 +9,7 @@ app.use(express.json());
 
 connectDB();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-const createProduct = async (req, res) => {
-  const {
-    product_name,
-    product_price,
-    isInStock,
-    category,
-    product_description,
-  } = req.body;
-  try {
-    const product = await ProductModel.create({
-      product_name,
-      product_price,
-      isInStock,
-      category,
-      product_description,
-    });
-    res.status(201).json({ message: "Product created", product });
-  } catch (error) {
-    res.status(400).json({ message: "something went wrong" });
-  }
-};
-
-app.post("/createProduct", createProduct);
-
-const getAllProduct = async (req, res) => {
-  try {
-    const products = await ProductModel.find();
-    res.json(products);
-  } catch (error) {
-    res.status(400).json({ message: "something went wrong" });
-  }
-};
-app.get("/getAllProducts", getAllProduct);
+app.use("/shopApi/V1/", productRoutes);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
