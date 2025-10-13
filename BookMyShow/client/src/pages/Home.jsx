8bcaecb287be59cf0164068f854aff9233957fcb
kachwalUttamsharma <kachwaluttamsharma@gmail.com>
2025-10-13 09:31:49 +0530

    movies api and protected route

--- BookMyShow/client/src/pages/Home.jsx
+++ BookMyShow/client/src/pages/Home.jsx
@@ -1,42 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate, Link } from "react-router-dom";
-import { message } from "antd";
-import { GetCurrentUser } from "../api/user";
-const Home = () => {
-  const [userInfo, setUserInfo] = useState();
-  const navigate = useNavigate();
+import React from "react";
 
-  const getValidUser = async () => {
-    try {
-      const response = await GetCurrentUser();
-      if (response.success) {
-        setUserInfo(response?.data);
-      }
-    } catch (error) {
-      message.error(error);
-    }
-  };
-  useEffect(() => {
-    if (localStorage.getItem("tokenForBMS")) {
-      // token is present
-      getValidUser();
-    } else {
-      navigate("/login");
-    }
-  }, []);
+const Home = () => {
   return (
     <>
       <div>Home</div>
-      <div>Hello : {userInfo?.name}</div>
-      <div>Email : {userInfo?.email}</div>
-      <Link
-        to="/login"
-        onClick={() => {
-          localStorage.removeItem("tokenForBMS");
-        }}
-      >
-        Logout
-      </Link>
     </>
   );
 };
