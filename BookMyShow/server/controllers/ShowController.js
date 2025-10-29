7386cb89fc28fab8dade05d25e53763f9d1133aa
kachwalUttamsharma <kachwaluttamsharma@gmail.com>
2025-10-29 09:19:16 +0530

    shows api and ui components

---
+++ BookMyShow/server/controllers/ShowController.js
@@ -0,0 +1,84 @@
+const ShowModel = require("../models/showSchema");
+
+const addShow = async (req, res, next) => {
+  try {
+    const newShow = new ShowModel(req.body);
+    await newShow.save();
+    res.send({
+      success: true,
+      message: "New show has been added!",
+    });
+  } catch (error) {
+    res.status(400);
+    next(error);
+  }
+};
+
+const deleteShow = async (req, res, next) => {
+  try {
+    const showId = req.params.showId;
+    await ShowModel.findByIdAndDelete(showId);
+    res.send({
+      success: true,
+      message: "The show has been deleted!",
+    });
+  } catch (error) {
+    res.status(400);
+    next(error);
+  }
+};
+
+const updateShow = async (req, res, next) => {
+  try {
+    await ShowModel.findByIdAndUpdate(req.body.showId, req.body);
+    res.send({
+      success: true,
+      message: "The show has been updated!",
+    });
+  } catch (error) {
+    res.status(400);
+    next(error);
+  }
+};
+
+const getAllShowsByTheatre = async (req, res, next) => {
+  try {
+    const theatreId = req.params.theatreId;
+    const shows = await ShowModel.find({ theatre: theatreId })
+      .populate("movie")
+      .populate("theatre");
+    res.send({
+      success: true,
+      message: "All shows are fetched",
+      data: shows,
+    });
+  } catch (error) {
+    res.status(400);
+    next(error);
+  }
+};
+
+const getAllShowsByMovie = async (req, res, next) => {
+  try {
+  } catch (error) {
+    res.status(400);
+    next(error);
+  }
+};
+
+const getAllShowsById = async (req, res, next) => {
+  try {
+  } catch (error) {
+    res.status(400);
+    next(error);
+  }
+};
+
+module.exports = {
+  addShow,
+  deleteShow,
+  updateShow,
+  getAllShowsById,
+  getAllShowsByMovie,
+  getAllShowsByTheatre,
+};
