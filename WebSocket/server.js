75ddcb25ebff033a0d74ce97d2f290122430b32f
kachwalUttamsharma <kachwaluttamsharma@gmail.com>
2025-11-12 07:55:11 +0530

    group mesaging

--- WebSocket/server.js
+++ WebSocket/server.js
@@ -26,12 +26,44 @@ io.on("connection", (socket) => {
 
   socket.on("create_group", (roomId) => {
     if (!chatRooms[roomId]) {
-      chatRooms[roomId] = { members: [] };
+      chatRooms[roomId] = { members: [], messages: [] };
     }
     socket.join(roomId);
-    chatRooms[roomId].members.push(socket.id);
+    if (!chatRooms[roomId].members.includes(socket.id)) {
+      chatRooms[roomId].members.push(socket.id);
+    }
     console.log(chatRooms);
     io.emit("update_groups_list", Object.keys(chatRooms));
+    io.to(roomId).emit("updated_members_list", chatRooms[roomId].members);
+  });
+
+  socket.on("join_group", (roomId) => {
+    console.log(socket.id, "joined the room : ", roomId);
+    if (chatRooms[roomId] && !chatRooms[roomId].members?.includes(socket.id)) {
+      chatRooms[roomId].members.push(socket.id);
+      socket.join(roomId);
+    }
+    io.to(roomId).emit("updated_members_list", chatRooms[roomId].members);
+  });
+
+  socket.on("leave_group", (roomId) => {
+    console.log(socket.id, "leaving the room : ", roomId);
+    if (chatRooms[roomId] && chatRooms[roomId].members?.includes(socket.id)) {
+      chatRooms[roomId].members.filter((id) => id != socket.id);
+      socket.leave(roomId);
+    }
+    io.to(roomId).emit("updated_members_list", chatRooms[roomId].members);
+  });
+
+  socket.on("group_message", (data) => {
+    console.log("Group message received:", data.message);
+    // update the chat room array
+    const msg = {
+      sender: socket.id,
+      message: data.message,
+    };
+    chatRooms[data.roomId].messages.push(msg);
+    io.to(data.roomId).emit("receive_group_message", msg);
   });
 });
 
